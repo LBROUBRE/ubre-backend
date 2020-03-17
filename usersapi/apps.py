9c12c3d3a7f6a8cb77f753d582e531a34842b536
Sergio <arcaymallo@gmail.com>
2020-03-17 11:47:52 +0100

    initial server + users app

---
+++ usersapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UsersapiConfig(AppConfig):
+    name = 'usersapi'
---
+++ usersapi/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(
+            queryset,
+            pk=self.kwargs['pk']
+        )
+        return obj
+
+#class Route(generics.ListCreateAPIView): #TODO
